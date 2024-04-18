4b680cf636c02a0898a367fc5481c607522ec619
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-04-18 20:45:01 +0900

    00_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <Novice.h>
 
 #include <Matrix.h>
+#include <Vector3D.h>
 #include <Debug.h>
+#include <Transform3D.h>
 
 const char kWindowTitle[] = "LE2A_14_ハマヤ_タイセイ_MT3";
 
@@ -15,21 +17,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Matrix4x4 m1 = {
-		{ 3.2f, 0.7f, 9.6f, 4.4f },
-		{ 5.5f,1.3f, 7.8f, 2.1f },
-		{ 6.9f, 8.0f, 2.6f, 1.0f },
-		{ 0.5f, 7.2f, 5.1f, 3.3f  },
+	Vector3 translate{ 4.1f, 2.6f, 0.8f };
+	Vector3 scale{ 1.5f, 5.2f, 7.3f };
+	Matrix4x4 translateMatrix = Transform3D::MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = Transform3D::MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f, 3.8f, 1.4f };
+	Matrix4x4 transformMatrix = {
+		{1.0f, 2.0f, 3.0f, 4.0f},
+		{3.0f, 1.0f, 1.0f, 2.0f },
+		{1.0f, 4.0f, 2.0f, 3.0f},
+		{2.0f, 2.0f, 1.0f, 3.0f}
 	};
-	Matrix4x4 m2{
-		{4.1f, 6.5f, 3.3f, 2.2f},
-		{8.8f, 0.6f, 9.9f, 7.7f},
-		{1.1f, 5.5f, 6.6f, 0.0f},
-		{3.3f, 9.9f, 8.8f, 2.2f}
-	};
-
+	Vector3 transformed = Transform3D::Homogeneous(point, transformMatrix); 
 	const int kRowHeight = 20;
-	const int kColumnWidth = 60;
 
 	// ---------------------------------------------ゲームループ---------------------------------------------
 	while (Novice::ProcessMessage() == 0) {
@@ -44,14 +44,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ----------------------------------------更新処理ここから----------------------------------------
 		///
 
-		Matrix4x4 resultAdd = m1 + m2;
-		Matrix4x4 resultMultiply = m1 * m2;
-		Matrix4x4 resultSubtract = m1 - m2;
-		Matrix4x4 inverseM1 = m1.inverse();
-		Matrix4x4 inverseM2 = Matrix4x4::Inverse(m2);
-		Matrix4x4 transposeM1 = m1.transpose();
-		Matrix4x4 transposeM2 = Matrix4x4::Transpose(m2);
-		Matrix4x4 identity = Matrix4x4::identity;
+
 
 		///
 		/// ----------------------------------------更新処理ここまで----------------------------------------
@@ -62,15 +55,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 
-
-		Debug::PrintMatrix4x4(0, 0, resultAdd);
-		Debug::PrintMatrix4x4(0, kRowHeight * 5, resultSubtract);
-		Debug::PrintMatrix4x4(0, kRowHeight * 5 * 2, resultMultiply);
-		Debug::PrintMatrix4x4(0, kRowHeight * 5 * 3, inverseM1);
-		Debug::PrintMatrix4x4(0, kRowHeight * 5 * 4, inverseM2);
-		Debug::PrintMatrix4x4(kColumnWidth * 5, 0, transposeM1);
-		Debug::PrintMatrix4x4(kColumnWidth * 5, kRowHeight * 5, transposeM2);
-		Debug::PrintMatrix4x4(kColumnWidth * 5, kRowHeight * 10, identity);
+		Debug::PrintVec3(0, 0, transformed);
+		Debug::PrintMatrix4x4(0, kRowHeight  * 2, translateMatrix);
+		Debug::PrintMatrix4x4(0, kRowHeight * 7, scaleMatrix);
 
 		///
 		/// ----------------------------------------描画処理ここまで----------------------------------------
