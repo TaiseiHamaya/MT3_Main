d3703ce6b96c0c53aa0877286214f51de4ceb6c8
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-05-17 11:40:51 +0900

    MT3_02_02_確認課題

--- main.cpp
+++ main.cpp
@@ -16,9 +16,15 @@
 
 const char kWindowTitle[] = "LE2A_14_ハマヤ_タイセイ_MT3";
 
-bool IsCollision(const Sphere& s1, const Sphere& s2) {
-	return (s1.get_transform().get_translate() - s2.get_transform().get_translate()).length() <= s1.get_radius() + s2.get_radius();
+struct Plane {
+	Vector3 normal; // 法線
+	float distance; // 距離
 };
+
+bool IsCollision(const Plane& plane, const Sphere& sphere) {
+	float distance = std::abs(Vector3::DotProduct(plane.normal, sphere.get_transform().get_translate()) - plane.distance);
+	return distance <= sphere.get_radius();
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -34,8 +40,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Sphere sphere1{ Transform3D{ Vec3::kBasis, Vec3::kZero, {-1.5f, 0.6f, 0.6f }}, Color{ 1.0f, 1.0f, 1.0f, 1.0f }, 1.f, 16 };
-	Sphere sphere2{ Transform3D{ Vec3::kBasis, Vec3::kZero, Vec3::kZero }, Color{ 1.0f, 1.0f, 1.0f, 1.0f }, 1.f, 16 };
+	Sphere sphere{ Transform3D{}, Color{ 1.0f, 1.0f, 1.0f, 1.0f }, 1.f, 16 };
+	Plane plane{ Vec3::kBasisY, 1.0f };
 
 	// ---------------------------------------------ゲームループ---------------------------------------------
 	while (Novice::ProcessMessage() == 0) {
@@ -50,33 +56,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ----------------------------------------更新処理ここから----------------------------------------
 		///
 
-		// s1
-		sphere1.begin();
+		// sphere
+		sphere.begin();
 		ImGui::SetNextWindowSize(ImVec2{ 300,210 }, ImGuiCond_Once);
 		ImGui::SetNextWindowPos(ImVec2{ 900, 100 }, ImGuiCond_Once);
 		ImGui::Begin("Sphere1", nullptr, ImGuiWindowFlags_NoSavedSettings);
-		sphere1.debug_gui();
+		sphere.debug_gui();
 		ImGui::End();
-		sphere1.update();
-		sphere1.begin_rendering();
+		sphere.update();
+		sphere.begin_rendering();
 
-		// s2
-		sphere2.begin();
+		// plane
 		ImGui::SetNextWindowSize(ImVec2{ 300,210 }, ImGuiCond_Once);
 		ImGui::SetNextWindowPos(ImVec2{ 900, 350 }, ImGuiCond_Once);
 		ImGui::Begin("Sphere2", nullptr, ImGuiWindowFlags_NoSavedSettings);
-		sphere2.debug_gui();
+		if (ImGui::DragFloat3("DirectionRotateV1", &plane.normal.x, 0.01f)) {
+			plane.normal = plane.normal.normalize();
+		}
+		Vector3 rotate = Vec3::kZero;
+		if (ImGui::DragFloat3("DirectionRotateV2", &rotate.x, 0.02f)) {
+			plane.normal = Transform3D::Homogeneous(plane.normal, Quaternion{ rotate }.to_matrix());
+			plane.normal = plane.normal.normalize();
+		}
+		if (ImGui::Button("ResetNormal")) {
+			plane.normal = Vec3::kBasisY;
+		}
+		ImGui::Text("%.3f, %.3f, %.3f", plane.normal.x, plane.normal.y, plane.normal.z);
+		ImGui::DragFloat("Distance", &plane.distance, 0.1f);
 		ImGui::End();
-		sphere2.update();
-		sphere2.begin_rendering();
 
-		if (IsCollision(sphere1, sphere2)) {
-			sphere1.set_color({ 1.0f,0.0f,0.0f,1.0f });
-			sphere2.set_color({ 1.0f,0.0f,0.0f,1.0f });
+		if (IsCollision(plane, sphere)) {
+			sphere.set_color({ 1.0f,0.0f,0.0f,1.0f });
 		}
 		else {
-			sphere1.set_color({ 1.0f,1.0f,1.0f,1.0f });
-			sphere2.set_color({ 1.0f,1.0f,1.0f,1.0f });
+			sphere.set_color({ 1.0f,1.0f,1.0f,1.0f });
 		}
 
 		// カメラ関連
@@ -92,9 +105,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		Debug::Grid3D();
-		sphere1.draw();
-		sphere2.draw();
+		sphere.draw();
+		{
+			Vector3 center = plane.normal * plane.distance;
+			std::array<Vector3, 4> planeVertexes;
+			planeVertexes[0] =
+				plane.normal.x != 0 || plane.normal.y != 0
+				? Vector3{ -plane.normal.y, plane.normal.x, 0.0f } : Vector3{ 0.0f, -plane.normal.z, plane.normal.y };
+			planeVertexes[0] = planeVertexes[0].normalize() * 10;
+			planeVertexes[2] = -planeVertexes[0];
+			planeVertexes[1] = Vector3::CrossProduct(planeVertexes[0], plane.normal);
+			planeVertexes[3] = -planeVertexes[1];
 
+
+			std::array<Vector3, 4> planeScreenVertexes;
+
+			for (int i = 0; i < planeVertexes.size(); ++i) {
+				planeScreenVertexes[i] = Transform3D::Homogeneous(planeVertexes[i] + center, Camera3D::GetVPOVMatrix());
+			}
+
+			Renderer::DrawLine(planeScreenVertexes[0], planeScreenVertexes[1], WHITE);
+			Renderer::DrawLine(planeScreenVertexes[1], planeScreenVertexes[2], WHITE);
+			Renderer::DrawLine(planeScreenVertexes[2], planeScreenVertexes[3], WHITE);
+			Renderer::DrawLine(planeScreenVertexes[3], planeScreenVertexes[0], WHITE);
+
+		}
 
 		///
 		/// ----------------------------------------描画処理ここまで----------------------------------------
