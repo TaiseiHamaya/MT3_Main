288bd94eab7acd3280577c14fed0186d683b6eec
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2024-07-04 23:55:27 +0900

    MT3_03_01_確認課題

--- main.cpp
+++ main.cpp
@@ -15,14 +15,6 @@
 
 const char kWindowTitle[] = "LE2A_14_ハマヤ_タイセイ_MT3";
 
-Vector3 CatmullRom(const Vector3& p0, const Vector3& p1, const Vector3& p2, const Vector3& p3, float t) {
-	return (
-		(-p0 + p1 * 3 - p2 * 3 + p3) * t * t * t +
-		(p0 * 2 - p1 * 5 + p2 * 4 - p3) * t * t +
-		(-p0 + p2) * t +
-		p1 * 2) / 2;
-}
-
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -40,30 +32,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Color color = { 1.0f,1.0f,1.0f,1.0f };
 
-	std::array<Vector3, 4> controlPoint{
-		Vector3{-0.8f, 0.58f, 1.0f},
-		Vector3{1.76f, 1.0f, -0.3f},
-		Vector3{0.94f, -0.7f, 2.3f},
-		Vector3{-0.53f, 0.26f, -0.15f}
-	};
-
-	std::array<Sphere, 4> controlPointSphere{
-		Sphere{Transform3D{}, color, 0.1f, 8},
-		Sphere{Transform3D{}, color, 0.1f, 8},
-		Sphere{Transform3D{}, color, 0.1f, 8},
-		Sphere{Transform3D{}, color, 0.1f, 8},
-	};
-	controlPointSphere[0].get_transform().set_translate(controlPoint[0]);
-	controlPointSphere[1].get_transform().set_translate(controlPoint[1]);
-	controlPointSphere[2].get_transform().set_translate(controlPoint[2]);
-	controlPointSphere[3].get_transform().set_translate(controlPoint[3]);
-
-	std::vector<Vector3> spline;
-	std::vector<Vector3> splineScreen;
-
-	constexpr int kDivision = 30;
-	spline.resize(kDivision * (controlPoint.size() - 1) + 1);
-	splineScreen.resize(kDivision * (controlPoint.size() - 1) + 1);
+	Transform3D shoulder{ Vec3::kBasis, Quaternion{0,0,-6.8f},{0.2f, 1.0f, 0.0f} };
+	Vector3 shoulderWorldPosition;
+	Sphere shoulderScreen{ {},{1.0f,0.0f,0.0f,1.0f},0.1f, 16 };
+	Transform3D elbow{ Vec3::kBasis, Quaternion{0,0,-1.4f},{0.4f, 0.0f, 0.0f} };
+	Vector3 elbowWorldPosition;
+	Sphere elbowScreen{ {},{0.0f,1.0f,0.0f,1.0f},0.1f, 16 };
+	Transform3D hand{ Vec3::kBasis, Quaternion{0,0,0},{0.3f, 0.0f, 0.0f} };
+	Vector3 handWorldPosition;
+	Sphere handScreen{ {},{0.0f,0.0f,1.0f,1.0f},0.1f, 16 };
 
 	// ---------------------------------------------ゲームループ---------------------------------------------
 	while (Novice::ProcessMessage() == 0) {
@@ -82,48 +59,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Camera3D::DebugGUI();
 		Camera3D::CameraUpdate();
 
-		// ControlPointのImGui
-		ImGui::Begin("ControlPoints");
-		// 始点
-		if (ImGui::DragFloat3("Point0", &controlPoint[0].x, 0.05f)) {
-			controlPointSphere[0].get_transform().set_translate(controlPoint[0]);
+		ImGui::Begin("Window");
+		if (ImGui::TreeNodeEx("Shoulder", ImGuiTreeNodeFlags_DefaultOpen)) {
+			shoulder.debug_gui();
+			ImGui::TreePop();
 		}
-
-		// 中間
-		if (ImGui::DragFloat3("Point1", &controlPoint[1].x, 0.05f)) {
-			controlPointSphere[1].get_transform().set_translate(controlPoint[1]);
+		ImGui::Separator();
+		if (ImGui::TreeNodeEx("Elbow", ImGuiTreeNodeFlags_DefaultOpen)) {
+			elbow.debug_gui();
+			ImGui::TreePop();
 		}
-
-		// 中間2
-		if (ImGui::DragFloat3("Point2", &controlPoint[2].x, 0.05f)) {
-			controlPointSphere[2].get_transform().set_translate(controlPoint[2]);
-		}
-
-		// 終点
-		if (ImGui::DragFloat3("Point3", &controlPoint[3].x, 0.05f)) {
-			controlPointSphere[3].get_transform().set_translate(controlPoint[3]);
+		ImGui::Separator();
+		if (ImGui::TreeNodeEx("Hand", ImGuiTreeNodeFlags_DefaultOpen)) {
+			hand.debug_gui();
+			ImGui::TreePop();
 		}
 		ImGui::End();
 
-		// ベジエの更新
-		for (int i = 0; i < kDivision; ++i) {
-			spline[i] = CatmullRom(controlPoint[0], controlPoint[0], controlPoint[1], controlPoint[2], float(i) / kDivision);
-		}
-		for (int i = 0; i < kDivision; ++i) {
-			spline[i + kDivision] = CatmullRom(controlPoint[0], controlPoint[1], controlPoint[2], controlPoint[3], float(i) / kDivision);
-		}
-		for (int i = 0; i < kDivision; ++i) {
-			spline[i + kDivision * 2] = CatmullRom(controlPoint[1], controlPoint[2], controlPoint[3], controlPoint[3], float(i) / kDivision);
-		}
-		spline[kDivision * 3] = CatmullRom(controlPoint[1], controlPoint[2], controlPoint[3], controlPoint[3], 1);
-		for (int i = 0; i < kDivision * 3 + 1; ++i) {
-			splineScreen[i] = Transform3D::Homogeneous(spline[i], Camera3D::GetVPOVMatrix());
-		}
+		shoulder.update();
+		elbow.update();
+		hand.update();
 
-		controlPointSphere[0].begin_rendering();
-		controlPointSphere[1].begin_rendering();
-		controlPointSphere[2].begin_rendering();
-		controlPointSphere[3].begin_rendering();
+		shoulderWorldPosition = Transform3D::Homogeneous(Vec3::kZero, shoulder.get_matrix());
+		elbowWorldPosition = Transform3D::Homogeneous(Vec3::kZero, elbow.get_matrix() * shoulder.get_matrix());
+		handWorldPosition = Transform3D::Homogeneous(Vec3::kZero, hand.get_matrix()* elbow.get_matrix()* shoulder.get_matrix());
+
+		shoulderScreen.get_transform().set_translate(shoulderWorldPosition);
+		elbowScreen.get_transform().set_translate(elbowWorldPosition);
+		handScreen.get_transform().set_translate(handWorldPosition);
+
+		shoulderScreen.begin_rendering();
+		elbowScreen.begin_rendering();
+		handScreen.begin_rendering();
 
 		///
 		/// ----------------------------------------更新処理ここまで----------------------------------------
@@ -134,13 +101,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		Debug::Grid3D();
-		for (int i = 0; i < kDivision * 3; ++i) {
-			Renderer::DrawLine(splineScreen[i], splineScreen[i + 1], color.hex());
-		}
-		controlPointSphere[0].draw();
-		controlPointSphere[1].draw();
-		controlPointSphere[2].draw();
-		controlPointSphere[3].draw();
+
+		shoulderScreen.draw();
+		elbowScreen.draw();
+		handScreen.draw();
 
 		///
 		/// ----------------------------------------描画処理ここまで----------------------------------------
